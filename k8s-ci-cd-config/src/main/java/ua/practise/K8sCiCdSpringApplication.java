8d301cb7a444f4b1fa9175ec512b8940e2be807f
Denys Ryhal <denysryhal24.@gmail.com>
2025-08-19 14:00:06 +0300

    created moduled and common plugins

--- k8s-ci-cd-config/src/main/java/ua/practise/K8sCiCdSpringApplication.java
+++ k8s-ci-cd-config/src/main/java/ua/practise/K8sCiCdSpringApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class K8sCiCdSpringApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(K8sCiCdSpringApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(K8sCiCdSpringApplication.class, args);
+  }
 }
